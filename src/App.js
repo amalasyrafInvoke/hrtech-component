ee8a538b817fb16130a5920868e52c5249d1ded8
amalasyrafs <57763629+amalasyrafs@users.noreply.github.com>
2022-01-25 16:16:02 +0800

    add the screen pages UI and functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import FullCalendar from './components/fullCalendar';
-// import DashboardWrapper from './components/dashboard';
+// import FullCalendar from './components/fullCalendar';
+import DashboardWrapper from './components/dashboard';
+// import LoadingScreen from './components/screens/loadingScreen';
+// import SuccessfulScreen, {
+//   ExampleAdditionalText,
+// } from './components/screens/successfulScreen';
 
 function App() {
-  return <FullCalendar />;
+  return (
+    // <LoadingScreen
+    //   text='Leave Request Created'
+    // />
+    // <SuccessfulScreen
+    //   text='Leave Request Created'
+    //   additionalText={<ExampleAdditionalText />}
+    // />
+    <DashboardWrapper />
+  );
 }
 
 export default App;
